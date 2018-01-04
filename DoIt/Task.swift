3fec8bfdd0dc9407e559545a82c8d35674b15621
Brandon Bezanson <brandon_bezanson@hotmail.com>
2018-01-04 01:19:02 -0700

    [4] Task Class Complete

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Brandon Bezanson on 2018-01-04.
+//  Copyright © 2018 Brandon Bezanson. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
